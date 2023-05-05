01578e268a54790fe9cdb39c8754a9a513066867
Tweizh <ghnmqdtg@gmail.com>
2023-05-05 17:00:54 +0800

    Feat: Use pre-trained ResNet50 to train on CIFAR10 and also introduce `tdqm`

--- cifar10_classifier.py
+++ cifar10_classifier.py
@@ -1,21 +1,34 @@
 import wandb
 import datetime
 import numpy as np
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.data.dataloader import default_collate
 import torchvision
 import torchvision.transforms as transforms
 
 
 def prepare_dataset(batch_size):
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    """
+    Prepare CIFAR10 dataset
+
+    Input:
+        batch_size: batch size for training and testing
+    Output:
+        train_loader: training data loader
+        test_loader: testing data loader
+        classes: class names
+    """
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    ])
 
     train_set = torchvision.datasets.CIFAR10(
         root='./data',
@@ -35,7 +48,7 @@ def prepare_dataset(batch_size):
         train_set,
         batch_size=batch_size,
         shuffle=True,
-        num_workers=48,
+        num_workers=1,
         pin_memory=True
     )
 
@@ -43,7 +56,7 @@ def prepare_dataset(batch_size):
         test_set,
         batch_size=batch_size,
         shuffle=True,
-        num_workers=48,
+        num_workers=1,
         pin_memory=True
     )
 
@@ -53,46 +66,50 @@ def prepare_dataset(batch_size):
     return train_loader, test_loader, classes
 
 
-class Net(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-        # 3 input channel, 6 output channel, 5x5 square convolution
-        # nn.Conv2d args: (in_channels, out_channels, kernel_size)
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        # Max pooling over a (2, 2) window
-        self.pool = nn.MaxPool2d(2, 2)
-        # 6 input channel, 16 output channel, 5x5 square convolution
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        # Fully connected layer 1
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        # Fully connected layer 2
-        self.fc2 = nn.Linear(120, 84)
-        # Fully connected layer 3
-        self.fc3 = nn.Linear(84, 10)
+def load_model(model_path=None):
+    """
+    Load pre-trained ResNet50 model
 
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        # flatten args: (input, start_dim=0, end_dim=-1)
-        # Set start_dim=1 so it will flatten all dimensions except batch
-        x = torch.flatten(x, 1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+    Input:
+        model_path: path to the model weights
+    Output:
+        model: ResNet50 model
+    """
+    # Load pre-trained ResNet50 model
+    model = torchvision.models.resnet50(weights='ResNet50_Weights.DEFAULT')
+    # Modify the last layer to fit CIFAR10 dataset, since it was trained on ImageNet (1000 classes)
+    model.fc = nn.Linear(model.fc.in_features, 10)
+    model = model.to(device)
+    if model_path:
+        model.load_state_dict(torch.load(model_path))
+    return model
 
 
 def train(model_path, train_loader, test_loader):
+    """
+    Train the model
+
+    Input:
+        model_path: path to save the model weights
+        train_loader: training data loader
+        test_loader: testing data loader
+    Output:
+        model: trained model
+    """
     print(f'\nStart training on {device}')
-    model = Net().to(device)
+    # Load pre-trained ResNet50 model
+    model = load_model()
+    # Define loss function and optimizer
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-
+    # Watch model in wandb
     wandb.watch(model, criterion, log="all")
+    # Set number of epochs
     num_epochs = 5
     for epoch in range(num_epochs):
         running_loss = 0.0
-        for i, batch in enumerate(train_loader):
+        pbar = tqdm(enumerate(train_loader), total=len(train_loader))
+        for i, batch in pbar:
             # Get the inputs
             inputs, labels = batch
             inputs = inputs.to(device)
@@ -107,44 +124,50 @@ def train(model_path, train_loader, test_loader):
 
             # Print statistics
             running_loss += loss.item()
+            # Update tqdm progress bar
+            pbar.set_description(
+                f'Epoch {epoch+1}/{num_epochs}, loss={running_loss/(i+1):.4f}')
 
-            # print every 2000 mini-batches
-            if i % 2000 == 1999:
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 2000))
-                wandb.log({'epoch': epoch+1, 'loss': running_loss/2000})
+            # print every 100 mini-batches
+            if i % 100 == 99:
+                wandb.log({'epoch': epoch+1, 'loss': running_loss/100})
                 running_loss = 0.0
 
-        running_loss = 0.0
+        # Compute accuracy on test set
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for data in test_loader:
+                images, labels = data
+                images = images.to(device)
+                labels = labels.to(device)
+                outputs = model(images)
+                _, predictions = torch.max(outputs, 1)
+                # labels.size() allows you to check out the shape of tensor `labels`
+                # labels.size(0) is the number of labels in the batch
+                total += labels.size(0)
+                correct += (predictions == labels).sum().item()
 
+        accuracy = 100 * correct / total
+        print(f'Accuracy of the network on the test images: {accuracy:.2f} %')
+        wandb.log({'epoch': epoch+1, 'accuracy': accuracy})
+
+    # Save model weights
     torch.save(model.state_dict(), model_path)
     print(f'Finished Training, model saved to {model_path}')
 
     return model
 
 
-def predict(model, batch_size):
-    print(f'\nStarting prediction using {device}')
-    # Get some random testing images
-    dataiter = iter(test_loader)
-    images, labels = next(dataiter)
-    images = images.to(device)
-    labels = labels.to(device)
-    # Save the grid of images
-    img_grid = torchvision.utils.make_grid(images)
-    with open('./imgs/test_grid.png', 'wb') as f:
-        torchvision.utils.save_image(img_grid, f)
-    print('Groundtruth: ', ' '.join(
-        f'{classes[labels[j]]:5s}' for j in range(batch_size)))
-
-    outputs = model(images)
-    _, predicted = torch.max(outputs, 1)
-
-    print('Predicted: ', ' '.join(f'{classes[predicted[j]]:5s}'
-                                  for j in range(batch_size)))
-
-
 def eval(model):
+    """
+    Evaluate the model
+
+    Input:
+        model: trained model
+    Output:
+        None
+    """
     print(f'\nStarting evaluation using {device}')
     # Prepare to count predictions for each class
     correct_predictions_by_class = {classname: 0 for classname in classes}
@@ -158,13 +181,13 @@ def eval(model):
             labels = labels.to(device)
             outputs = model(images)
             _, predictions = torch.max(outputs, 1)
-            # collect the correct predictions for each class
+            # Collect the correct predictions for each class
             for label, prediction in zip(labels, predictions):
                 if label == prediction:
                     correct_predictions_by_class[classes[label]] += 1
                 total_predictions_by_class[classes[label]] += 1
 
-    # print accuracy for each class
+    # Print accuracy for each class
     for classname, correct_count in correct_predictions_by_class.items():
         accuracy = 100 * float(correct_count) / \
             total_predictions_by_class[classname]
@@ -177,14 +200,20 @@ if __name__ == '__main__':
                entity='ghnmqdtg',
                name=f'CIFAR-Net-{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}')
 
-    batch_size = 4
-    model_path = './cifar_net.pt'
+    # CIFAR10 dataset has 50000 training images and 10000 test images
+    # Set batch_size to 100, so we have 500 batches for training and 100 batches for testing
+    batch_size = 100
+    model_path = './cifar10_ResNet50.pt'
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f'device: {device}')
-
+    # Prepare dataset
     train_loader, test_loader, classes = prepare_dataset(batch_size=batch_size)
-    model = train(model_path=model_path,
-                  train_loader=train_loader, test_loader=test_loader)
-    predict(model, batch_size)
+    # Train the model
+    model = train(
+        model_path=model_path,
+        train_loader=train_loader,
+        test_loader=test_loader
+    )
+    # Evaluate the model
     eval(model)
